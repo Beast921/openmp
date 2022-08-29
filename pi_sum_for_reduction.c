029d55e473623f0e335a706bd93132184e6f2c04
chirag <chiragnair03@gmail.com>
2022-08-29 22:29:33 +0530

    bruh

---
+++ pi_sum_for_reduction.c
@@ -0,0 +1,24 @@
+#include<omp.h>
+#include<stdio.h>
+static long num_steps=100000;
+double step;
+#define NUM_THREADS 2
+void main(){
+    double pi=0.0;
+    step=1.0/(double) num_steps;
+    double start,end;
+    omp_set_num_threads(NUM_THREADS);
+    start=omp_get_wtime();
+    #pragma omp parallel
+    {
+        int i;
+        double x;
+        #pragma omp for reduction(+:pi)
+        for(i=0;i<num_steps;i++){
+            x=(i+0.5)*step;
+            pi+=4.0/(1.0+x*x)*step;
+        }
+    }
+    end=omp_get_wtime();
+    printf("%f %f",end-start,pi);
+}
